c048d32efde36a035f2d28db71c3e8d799d66c36
VraoNOVA <vivek.rao.vivek@gmail.com>
2020-07-29 15:25:12 -0400

    Update lambda.py
    Added connection pooling as well as exception handling.

--- lambda.py
+++ lambda.py
@@ -9,18 +9,45 @@ import pymysql
 
 from DBUtils.PooledDB import PooledDB
 
+#Database connection and pooling setup 
+rds_host  = "compugain1.cftwo5jbmlrm.us-east-1.rds.amazonaws.com"
+name = "compuGain1"
+password = "compuGain1"
+db_name = "compuGain1"
+mySQLConnectionPool = PooledDB(creator   = pymysql,     
+
+                               host      = rds_host,
+
+                               user      = name,
+
+                               password  = password,
+
+                               database  = db_name,
+
+                               autocommit    = True,
+
+                               charset       = "utf8mb4",
+
+                               cursorclass   = pymysql.cursors.DictCursor,
+
+                               blocking      = True,
+
+                               maxconnections = 5000)
+                               
+#Database conneciton setup ending 
+
 def lambda_handler(event, context):
-    print("In lambda handler")
-    print(event)
-    print (type(event))
-    print(event)
+  try:
+    #*****************************************************************
+    #Initializing all the data we will place into the DB
     millis = int(round(time.time() * 1000))
-    print (str(millis))
-    print("\n")
-    print(str(event["body"]))
     key1 = "insertTime"
     currentD =  (ast.literal_eval(str(event["body"])))
     currentD[key1] = str(millis)
+    global mySQLConnectionPool
+    one = mySQLConnectionPool.connection()
+    cursorOne = one.cursor()
+    cursorOne.execute("SELECT * FROM flights")
     YEAR = currentD['YEAR']
     MONTH = currentD['MONTH']
     DAY = currentD['DAY']
@@ -52,13 +79,6 @@ def lambda_handler(event, context):
     AIRLINE_DELAY  = currentD['AIRLINE_DELAY']
     LATE_AIRCRAFT_DELAY = currentD['LATE_AIRCRAFT_DELAY']
     WEATHER_DELAY = currentD['WEATHER_DELAY']
-    rds_host  = "appychip-instance-1.ckbuxwoy9jlx.us-east-2.rds.amazonaws.com"
-    name = "appychip"
-    password = "appychip"
-    db_name = "appychip"
-    connection = pymysql.connect(rds_host, user=name, passwd=password, db=db_name)
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM flights")
     insertDB = '''INSERT INTO flights (YEAR, MONTH, DAY, DAY_OF_WEEK, AIRLINE, FLIGHT_NUMBER , TAIL_NUMBER, ORIGIN_AIRPORT, DESTINATION_AIRPORT, SCHEDULED_DEPARTURE,                                                                                                                       
     DEPARTURE_TIME, DEPARTURE_DELAY, TAXI_OUT, WHEELS_OFF, SCHEDULED_TIME, ELAPSED_TIME, AIR_TIME, DISTANCE, WHEELS_ON, TAXI_IN,                                                                                                                                                            
     SCHEDULED_ARRIVAL, ARRIVAL_TIME, ARRIVAL_DELAY, DIVERTED, CANCELLED, CANCELLATION_REASON, AIR_SYSTEM_DELAY, SECURITY_DELAY,                                                                                                                                                             
@@ -67,11 +87,9 @@ def lambda_handler(event, context):
     DEPARTURE_TIME, DEPARTURE_DELAY, TAXI_OUT, WHEELS_OFF, SCHEDULED_TIME, ELAPSED_TIME, AIR_TIME, DISTANCE, WHEELS_ON, TAXI_IN,
     SCHEDULED_ARRIVAL, ARRIVAL_TIME, ARRIVAL_DELAY, DIVERTED, CANCELLED, CANCELLATION_REASON, AIR_SYSTEM_DELAY, SECURITY_DELAY,
     AIRLINE_DELAY, LATE_AIRCRAFT_DELAY, WEATHER_DELAY)
-    cursor.execute(insertDB , val)
-    connection.commit()
-    cnt = cursor.rowcount
-    connection.close()
-    currentD["count"] = cnt
+    cursorOne.execute(insertDB , val)
+    cursorOne.close()
+    one.close()
     resp = {
         "statusCode": 200,
         "headers": {
@@ -82,3 +100,15 @@ def lambda_handler(event, context):
     }
 
     return resp
+    
+except Exception as e:
+s = str(e)
+print(s)
+result = {
+        "statusCode": 403,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": s 
+                    }
+return result 
